c76468b72d85bbd5c3d7ba38a24a25917e133a5d
Afshin <>
2022-04-01 14:03:43 -0400

    navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,46 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import {Nav, Navbar, NavDropdown} from 'react-bootstrap'
+import { reSetAuthedUser } from '../actions/authedUser'
 
 
 function Navigation(props) {
+	const { user, dispatch } = props
+
+	const onLogoutBtnClick = () => {
+		dispatch(reSetAuthedUser())
+	}
+
+	return (
+		<Navbar bg="light">
+			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			<Navbar.Collapse id="responsive-navbar-nav">
+				<Nav>
+					<Nav.Link as={NavLink} to="/" exact> Home </Nav.Link>
+					<Nav.Link as={NavLink} to="/add"> New Question </Nav.Link>
+					<Nav.Link as={NavLink} to="/leaderboard"> Leaderboard </Nav.Link>
+					<NavDropdown title={
+						<div className="pull-right">
+							<img className="rounded-circle"
+							src={user.avatarURL}
+							style={{ width: '40px' }}
+							/>
+							{user.name}
+						</div>
+					} id="responsive-nav-dropdown">
+						<NavDropdown.Item onClick={onLogoutBtnClick}>Logout</NavDropdown.Item>
+					</NavDropdown>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	)
 }
 
-export default Navigation
+function mapStateToProps({ users, authedUser }) {
+	return {
+		user: users[authedUser]
+	}
+}
+
+export default connect(mapStateToProps)(Navigation)
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import reducer from './reducers'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
